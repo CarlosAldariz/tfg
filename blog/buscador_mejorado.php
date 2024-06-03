b05bfc715f3e2f1a7d4f4170574014fca8e9cd4c
CarlosAldariz <144796290+CarlosAldariz@users.noreply.github.com>
2024-06-03 18:25:08 +0200

    Add files via upload

---
+++ blog/buscador_mejorado.php
@@ -0,0 +1,58 @@
+<?php 
+
+require 'admin/config.php'; 
+require 'functions.php'; 
+
+$conexion = conexion($bd_config); 
+if (!$conexion) { 
+    header('Location: error.php'); 
+} 
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['busqueda'])) { 
+    $busqueda = limpiarDatos($_GET['busqueda']); 
+
+    $statement = $conexion->prepare( 
+        'SELECT * FROM articulos WHERE titulo LIKE :busqueda or text LIKE :busqueda'
+    ); 
+    $statement->execute(array(':busqueda' => "%$busqueda%")); 
+    $resultados = $statement ->fetchAll(); 
+
+    if (empty($resultados)) {  
+        if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['busqueda'])) { 
+            $busqueda = limpiarDatos($_GET['busqueda']); 
+        
+            $statement = $conexion->prepare( 
+                'SELECT * FROM playas WHERE nombre LIKE :busqueda or descripcion LIKE :busqueda'
+            ); 
+            $statement->execute(array(':busqueda' => "%$busqueda%")); 
+            $resultados = $statement ->fetchAll();  
+            if (empty($resultados)) {  
+                if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['busqueda'])) { 
+                    $busqueda = limpiarDatos($_GET['busqueda']); 
+                
+                    $statement = $conexion->prepare( 
+                        'SELECT * FROM categorias WHERE nombre LIKE :busqueda'
+                    ); 
+                    $statement->execute(array(':busqueda' => "%$busqueda%")); 
+                    $resultados = $statement ->fetchAll();  
+                } if (empty($resultados)) {  
+                    if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['busqueda'])) { 
+                        $busqueda = limpiarDatos($_GET['busqueda']); 
+                    
+                        $statement = $conexion->prepare( 
+                            'SELECT * FROM productos WHERE nombre LIKE :busqueda or descripcion LIKE :busqueda'
+                        ); 
+                        $statement->execute(array(':busqueda' => "%$busqueda%")); 
+                        $resultados = $statement ->fetchAll(); 
+                    }}}}}}  if (empty($resultados)) {
+                 $titulo = 'No se encontraron articulos con el resultado: ' . $busqueda;
+    } else { 
+        $titulo = 'Resultados de la busqueda: ' . $busqueda;
+} else { 
+    header('Location: ' . RUTA . '/index.php');
+} 
+
+require 'views/buscar.view.php';
+
+
+?>
