a61dd051f654ba6163591b5f37383acbaf845c1a
CarlosAldariz <144796290+CarlosAldariz@users.noreply.github.com>
2024-06-12 18:49:04 +0200

    Add files via upload

---
+++ blog/Index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <?php 
+
+    require 'admin/config.php'; 
+
+    require 'functions.php'; 
+
+    $conexion = conexion($bd_config); 
+
+    if (!$conexion) { 
+        header('Location: error.php');
+    } 
+
+    $posts = obtener_post($blog_config['post_por_pagina'], $conexion); 
+
+    if (!$posts) { 
+        header('Location: error.php');
+    } 
+
+    require 'views/index.view.php';  
+
+    ?>
+</body>
+</html>
