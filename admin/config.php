16664ae43a925ae817f23046fddf18c9485b73e7
CarlosAldariz <144796290+CarlosAldariz@users.noreply.github.com>
2024-06-20 18:41:33 +0200

    Add files via upload

---
+++ admin/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+    define('RUTA', 'http://localhost/php_udemy/blog/'); 
+
+$bd_config = array( 
+    'basedatos' => 'surfdamorte', 
+    'usuario' => 'root', 
+    'pass' => ''
+); 
+
+$blog_config = array( 
+    'post_por_pagina' => '6', 
+    'carpeta_imagenes' => 'imagenes/', 
+); 
+
+
+?>
