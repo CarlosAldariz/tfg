30e049326e920c107bfacb2be18c58c7b87f63c1
CarlosAldariz <144796290+CarlosAldariz@users.noreply.github.com>
2024-07-10 09:59:14 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<div class="footer">
+        <div class="footer-left">
+            <p><strong>LEGAL</strong></p>
+            <p><a href="<?php echo RUTA; ?>aviso_legal.php">Aviso Legal</a></p>
+            <p><a href="<?php echo RUTA; ?>aviso_legal.php">Política de privacidad y cookies</a></p>
+            <p><a href="<?php echo RUTA; ?>aviso_legal.php">Envío y pago</a></p>
+            <p><a href="<?php echo RUTA; ?>aviso_legal.php">Derecho de desestimiento</a></p>
+        </div>
+        <div class="footer-right">
+            <p><strong>INFORMACIÓN</strong></p>
+            <p><a href="<?php echo RUTA; ?>quienes_somos.php">Quienes somos</a></p>
+            <p><a href="Index.php">Revista</a></p>
+            <p><a href="Playas.php">Playas</a></p>
+            <p><a href="<?php echo RUTA; ?>/contacto/">Contacto</a></p>
+        </div> 
+</div> 
+
